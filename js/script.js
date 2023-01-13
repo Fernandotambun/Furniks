e3f1d54b4f6a25ad07cc06a37ddc11e71adc94be
Fernando Tambun <111202214723@mhs.dinus.ac.id>
2023-01-13 14:49:17 +0700

    Tugas Akhir

---
+++ js/script.js
@@ -0,0 +1,79 @@
+// init Isotope
+var $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+  });
+  
+  var filterBtns = $('.filter-button-group').find('button');
+  function resetFilterBtns(){
+    filterBtns.each(function(){
+      $(this).removeClass('active-filter-btn');
+    });
+  }
+
+  // external js: isotope.pkgd.js
+
+
+/* minus and plus */
+
+'use strict';
+$(document).ready(function() {
+
+    // Accordion
+    var all_panels = $('.templatemo-accordion > li > ul').hide();
+
+    $('.templatemo-accordion > li > a').click(function() {
+        console.log('Hello world!');
+        var target =  $(this).next();
+        if(!target.hasClass('active')){
+            all_panels.removeClass('active').slideUp();
+            target.addClass('active').slideDown();
+        }
+      return false;
+    });
+    // End accordion
+
+    // Product detail
+    $('.product-links-wap a').click(function(){
+      var this_src = $(this).children('img').attr('src');
+      $('#product-detail').attr('src',this_src);
+      return false;
+    });
+    $('#btn-minus').click(function(){
+      var val = $("#var-value").html();
+      val = (val=='1')?val:val-1;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+    $('#btn-plus').click(function(){
+      var val = $("#var-value").html();
+      val++;
+      $("#var-value").html(val);
+      $("#product-quanity").val(val);
+      return false;
+    });
+    $('.btn-size').click(function(){
+      var this_val = $(this).html();
+      $("#product-size").val(this_val);
+      $(".btn-size").removeClass('btn-secondary');
+      $(".btn-size").addClass('btn-success');
+      $(this).removeClass('btn-success');
+      $(this).addClass('btn-secondary');
+      return false;
+    });
+    // End roduct detail
+
+});
+
+  
+
+
+
+  
